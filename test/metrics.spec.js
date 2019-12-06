e82d6b233602d09e12087e16857d1a5453e269d6
Jamey Graham <jgraham@metlife.com>
2019-12-06 13:14:59 -0500

    broke metrics in to basic and enhanced services; brought in refactored TS package from AIM

--- test/metrics.spec.js
+++ test/metrics.spec.js
@@ -1,20 +1,18 @@
 const client = require('./it-client');
-const metrics = require('../lib/metrics')(client);
+const metrics = require('../lib/metrics/metrics')(client);
 
 const itApp = {
     id: 577,
     name: '0000-SANDBOX_HEALTHRULE_TEST'
 }
 
-// metrics.browse(itApp, 'Overall Application Performance').then(console.log)
+metrics.browseTree(itApp, 'Overall Application Performance')
+.then(console.log)
+.catch(console.log)
 
-// metrics.browse(itApp, 'Overall Application Performance|*|Individua? Nodes').then(console.log)
 
-// metrics.browse(itApp, 'Overall Application Performance').then(nodes => {
-//     console.log(nodes);
-//     metrics.fetchMetrics(itApp, nodes).then(console.log)
-// })
-
-//metrics.fetchMetrics(itApp, 'Overall Application Performance|Calls per Minute').then(console.log);
-
-metrics.fetchMetrics(itApp, 'Overall Application Performance|*|Calls per Minute').then(console.log);
+metrics.browseTree(itApp, 'Overall Application Performance')
+.then(nodes => {
+    nodes = nodes.filter(n => n.name=='Calls per Minute');
+    metrics.fetchMetricData(nodes).then(console.log);
+}).catch(console.log)
