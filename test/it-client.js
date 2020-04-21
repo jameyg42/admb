9948749da90a049dbf46df719be5ff5ce52a1f6f
Jamey Graham <jgraham@metlife.com>
2020-04-21 12:18:04 -0400

    stash modified test code

--- test/it-client.js
+++ test/it-client.js
@@ -1,7 +1,7 @@
 const client = require('@metlife/appd-client-js')
 
 module.exports = client.open({
-    url: 'https://dev.appd.metlife.com',
+    url: 'https://qa.appd.metlife.com',
     uid : 'AQ813306',
     pwd : 'gad8babU'
 })
--- test/metrics/metricsEx.spec.js
+++ test/metrics/metricsEx.spec.js
@@ -3,20 +3,27 @@ const metrics = require('../../lib/metrics/metricsEx')(client);
 const range = require('../../lib/range');
 
 const itApp = {
-    id: 577,
-    name: '0000-SANDBOX_HEALTHRULE_TEST'
+    id: 130,
+    name: '2942-IBSE_QA'
 }
 
-metrics.browse(itApp, 'Overall Application Performance').then(console.log)
+//metrics.browse(itApp, 'Overall Application Performance').then(console.log)
 
-metrics.browse(itApp, 'Overall Application Performance|*|Individua? Nodes').then(console.log)
+//metrics.browse(itApp, 'Overall Application Performance|*|Individua? Nodes').then(console.log)
 
-metrics.browse(itApp, 'Overall Application Performance').then(nodes => {
-//    console.log(nodes);
-    metrics.fetchMetrics(itApp, nodes).then(console.log)
-})
 
-metrics.fetchMetrics(itApp, 'Overall Application Performance|Calls per Minute').then(console.log);
+// metrics.browse(itApp, 'Overall Application Performance').then(nodes => {
+// //    console.log(nodes);
+//     metrics.fetchMetrics(itApp, nodes).then(console.log)
+// })
 
-metrics.fetchMetrics(itApp, 'Overall Application Performance|*|Calls per Minute', range.beforeNow(60))
+//metrics.fetchMetrics(itApp, 'Overall Application Performance|Calls per Minute').then(console.log);
+
+// metrics.fetchMetrics(itApp, 'Overall Application Performance|Calls per Minute', range.beforeNow(60))
+// .then(ms => console.log(ms[0].data, ms[0].data.length));
+
+// metrics.fetchBaselineMetrics(itApp, 'Overall Application Performance|Calls per Minute', range.beforeNow(60), 'DEFAULT')
+// .then(ms => console.log(ms[0].data, ms[0].data.length));
+
+metrics.fetchMetrics(itApp, 'Overall Application Performance|Calls per Minute', range.beforeNow(60))
 .then(ms => console.log(ms[0].data, ms[0].data.length));
