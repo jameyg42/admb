c8e20fd5cb0b6c9d781d4ec8a0535544da082f30
Jamey Graham <jgraham@metlife.com>
2020-10-13 12:33:46 -0400

    added Databases vnode

--- test/metrics-ex/metrics-ex.spec.js
+++ test/metrics-ex/metrics-ex.spec.js
@@ -7,6 +7,8 @@ const itApp = {
     name: '2942-IBSE_QA'
 }
 
+//metrics.browse(itApp, '').then(console.log)
+metrics.browse(itApp, 'Databases|ESEQ.METLIFE.COM-Oracle DB-USCKU1METE0183L.metnet.net-Oracle*').then(console.log)
 //metrics.browse(itApp, 'Overall Application Performance').then(console.log)
 
 //metrics.browse(itApp, 'Overall Application Performance|*|Individua? Nodes').then(console.log)
@@ -25,5 +27,5 @@ const itApp = {
 // metrics.fetchBaselineMetrics(itApp, 'Overall Application Performance|Calls per Minute', range.beforeNow(60), 'DEFAULT')
 // .then(ms => console.log(ms[0].data, ms[0].data.length));
 
-metrics.fetchMetrics(itApp, 'Overall Application Performance|IBSE*|Calls per Minute')
-.then(ms => console.log(ms[0].data, ms[0].data.length));
+// metrics.fetchMetrics(itApp, 'Overall Application Performance|IBSE*|Calls per Minute')
+// .then(ms => console.log(ms[0].data, ms[0].data.length));
